5f825ed9072dd94396adb7e43203c450dc82fbf7
𝐀𝐢𝐳𝐞𝐧-𝐒𝐚𝐦𝐚 <yashpacholi895@gmail.com>
2024-09-24 15:03:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,19 +93,32 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_photo(
-            photo= START_PIC,
-            caption= START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            
-        )
-        return
+        if START_PIC:
+            await message.reply_photo(
+                photo=START_PIC,
+                caption=START_MSG.format(
+                    first=message.from_user.first_name,
+                    last=message.from_user.last_name,
+                    username=None if not message.from_user.username else '@' + message.from_user.username,
+                    mention=message.from_user.mention,
+                    id=message.from_user.id
+                ),
+                reply_markup=reply_markup,
+            )
+            return
+        else:
+            await message.reply_text(
+                text=START_MSG.format(
+                    first=message.from_user.first_name,
+                    last=message.from_user.last_name,
+                    username=None if not message.from_user.username else '@' + message.from_user.username,
+                    mention=message.from_user.mention,
+                    id=message.from_user.id
+                ),
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+            )
+            return
 
     
 #=====================================================================================##
@@ -152,8 +165,9 @@ async def not_joined(client: Client, message: Message):
         id=message.from_user.id
     ),
     reply_markup=InlineKeyboardMarkup(buttons)
-)
-
+    )
+    return
+        
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
